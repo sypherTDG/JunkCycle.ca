47934bb6c4bde77c85070b853f3cf84a6cc695ee
sypherTDG <mac@mac.home>
2025-06-21 23:11:54 -0400

    Remove deprecated useUnifiedTopology option from Mongoose config

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,6 @@ app.use('/join-waitlist', limiter);
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
